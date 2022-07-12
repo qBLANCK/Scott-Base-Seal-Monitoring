0b908938dd364cccc0d1042d02d8c24beeda4897
fletcherd3 <fletcherjdick@gmail.com>
2022-07-13 11:23:33 +1200

    refactored seal detection dir

--- Models/Seals/detection/evaluate.py
+++ Models/Seals/detection/evaluate.py
@@ -1,286 +1,231 @@
-import torch
-from torch import Tensor
 import math
-import gc
-
+import torch
 import torch.nn.functional as F
 from torch.autograd import Variable
 
-from tools.image import index_map
-import tools.image.cv as cv
+from detection import box
+from tools import struct, const, pluck, shape
+import numpy as np
 
-import tools.confusion as c
+def bookend(*xs, dim=0):
+    def to_tensor(xs):
+        return xs if torch.is_tensor(xs) else torch.FloatTensor([xs])
+    return torch.cat([to_tensor(x) for x in xs], dim)
 
-from tools.image.transforms import normalize_batch
-from tools import struct, tensor, shape, cat_tables, shape_info, \
-    Histogram, ZipList, transpose_structs, transpose_lists, pluck, Struct, filter_none, split_table, tensors_to, map_tensors
-
-from detection import box, evaluate, detection_table
-from functools import reduce
-
-import operator
-
-
-def mean_results(results):
-    total = reduce(operator.add, results)
-    return total / total.size, total
-
-def sum_results(results):
-    return reduce(operator.add, results)
-
-# TODO: move this entirely to the individual object detector
-def make_statistics(data, encoder, loss, prediction):
-
-    stats = struct(error=sum(loss.values()),
-        loss = loss._map(Tensor.item),
-        size = data.image.size(0),
-        instances=data.lengths.sum().item(),
-    )
-
-    return stats
-
-
-
-def eval_train(model, encoder, debug = struct(), device=torch.cuda.current_device()):
-    def f(data):
-        image = data.image.to(device)
-        norm_data = normalize_batch(image)
-        prediction = model(norm_data)
-
-        target_table = tensors_to(data.target, device=device)
-        encoding = tensors_to(data.encoding, device=device)
-
-        targets = split_table(target_table, data.lengths.tolist())
-
-        input_size = (image.shape[2], image.shape[1])
-        loss = encoder.loss(input_size, targets, encoding, prediction)
-
-        statistics = make_statistics(data, encoder, loss, prediction)
-        return struct(error = sum(loss.values()) / image.data.size(0), statistics=statistics, size = data.image.size(0))
-    return f
-
-
-def summarize_train_stats(name, results, classes, log):
-    totals = sum_results(results)
-    avg = totals._subset('loss', 'instances', 'error') / totals.size
-
-    log.scalars(name + "/loss", avg.loss._extend(total = avg.error))
-
-    loss_str = " + ".join(["{} : {:.3f}".format(k, v) for k, v in sorted(avg.loss.items())])
-    return ('n: {}, instances : {:.2f}, loss: {} = {:.3f}'.format(totals.size, avg.instances, loss_str, avg.error))
-
-
-def summarize_train(name, results, classes, epoch, log):
-    summary = summarize_train_stats(name, results, classes, log)
-    print('{} epoch: {} {}'.format(name, epoch, summary))
-
-
-
-def evaluate_image(model, image, encoder, nms_params=detection_table.nms_defaults,  device=torch.cuda.current_device()):
-    model.eval()
-    with torch.no_grad():
-        batch = image.unsqueeze(0) if image.dim() == 3 else image          
-        assert batch.dim() == 4, "evaluate: expected image of 4d  [1,H,W,C] or 3d [H,W,C]"
-        input_size = (batch.shape[2], batch.shape[1])
-
-        norm_data = normalize_batch(batch.to(device)).contiguous()
-        prediction = map_tensors(model(norm_data), lambda p: p.detach()[0])
-
-        # print(shape(prediction))
-
-        return struct(detections = encoder.decode(input_size, prediction, nms_params=nms_params), prediction = prediction)
-
-  
-eval_defaults = struct(
-    overlap = 256,
-    split = False,
-
-    image_size = (600, 600),
-    batch_size = 1,
-    nms_params = detection_table.nms_defaults,
-
-    device=torch.cuda.current_device(),
-    debug = ()
-)  
-
-def evaluate_full(model, data, encoder, params=eval_defaults):
-    model.eval()
-    with torch.no_grad():
-        result = evaluate_image(model, data.image, encoder, device=params.device, nms_params=params.nms_params)
-
-        prediction = map_tensors(result.prediction, lambda p: p.unsqueeze(0))
-        target = tensors_to(data.target, device=params.device)
-        encoding = tensors_to(data.encoding, device=params.device)
-
-        input_size = (data.image.shape[2], data.image.shape[1])
-        loss = encoder.loss(input_size, [target], encoding, prediction)
-        statistics = make_statistics(data, encoder, loss, result.prediction)
-
-        return result._extend(statistics=statistics)
-
-
-def eval_test(model, encoder, params=eval_defaults):
-    def f(data):
-        result = evaluate_full(model, data, encoder, params)
-        return struct (
-            id = data.id,
-            target = data.target._map(Tensor.to, params.device),
-
-            detections = result.detections,
-
-            # for summary of loss
-            instances=data.lengths.sum().item(),
-            statistics = result.statistics,
-
-            size = data.image.size(0),
-        )
-    return f
-
-
-def percentiles(t, n=100):
+def max_1d(t):
     assert t.dim() == 1
-    return torch.from_numpy(np.percentile(t.numpy(), np.arange(0, n)))
-
-def mean(xs):
-    return sum(xs) / len(xs)
+    x, i = t.max(0)
+    return x.item(), i.item()
 
 
-def condense_pr(pr, n=400): 
-    positions = [0]
-    size = pr.false_positives.size(0)
-    i = 0
+def max_2d(t):
+    assert (t.dim() == 2)
+    values, inds = t.max(0)
+    value, i = values.max(0)
+    j = inds[i]
+    return value.item(), (i.item(), j.item())
 
-    for t in range(0, n):
-        while pr.recall[i] <= (t / n) and i < size:
-            i = i + 1
+def take_max_2d(t):
+    v, (i, j) = max_2d(t)
+    t[:, i] = 0
+    t[j, :] = 0
+    return v, (i, j)
 
-        if i < size:
-            positions.append(i)
+def match_targets(target1, target2, threshold=0.5):
+    ious = box.iou_matrix(target1.bbox, target2.bbox)
+    matches = []
 
-    t = torch.LongTensor(positions)
+    if ious.size(0) == 0 or ious.size(1) == 0:
+        return []
 
-    return struct (
-        recall = pr.recall[t],
-        precision = pr.precision[t],
-        confidence = pr.confidence[t],
+    value, loc = take_max_2d(ious)
+    while value > threshold:
+        matches.append(struct(match=loc, iou=value))
+        value, loc = take_max_2d(ious)
 
-        false_positives = pr.false_positives[t],
-        false_negatives = pr.false_negatives[t],
-        true_positives  = pr.true_positives[t]
-    )
-    
-
-def compute_thresholds(pr):
-    
-    f1 = 2 * (pr.precision * pr.recall) / (pr.precision + pr.recall)
-
-    def find_threshold(t):
-        diff = pr.false_positives - pr.false_negatives
-        p = int((t / 100) * pr.n)
-
-        zeros = (diff + p == 0).nonzero(as_tuple=False)
-        i = 0 if zeros.size(0) == 0 else zeros[0]
-
-        return pr.confidence[i].item()
-
-    margin = 10
-
-    return struct (
-        lower   = find_threshold(-margin), 
-        middle  = find_threshold(0), 
-        upper   = find_threshold(margin)
-    )
-
-def threshold_count(confidence, thresholds):
-    d = {k : (confidence > t).sum().item() for k, t in thresholds.items()}
-    return Struct(d)
+    return matches
 
 
 
-def count_target_classes(image_pairs, class_ids):
-    labels = torch.cat([i.target.label for i in image_pairs])
-    counts = labels.bincount(minlength = len(class_ids))
+def match_boxes(prediction, target,  threshold=0.5, eps=1e-7):
+    n = prediction.label.size(0)
+    matches = []
 
-    return {k : count for k, count in zip(class_ids, counts)}
+    ious = box.iou_matrix(prediction.bbox, target.bbox)
 
-def compute_AP(results, classes, conf_thresholds=None):
-
-    class_ids = pluck('id', classes)
-    iou_thresholds = list(range(30, 100, 5))
-
-    compute_mAP = evaluate.mAP_classes(results, num_classes = len(class_ids))
-    info = transpose_structs ([compute_mAP(t / 100) for t in iou_thresholds])
-
-    info.classes = transpose_lists(info.classes)
-    assert len(info.classes) == len(class_ids)
-
-    target_counts = count_target_classes(results, class_ids)
-
-    def summariseAP(ap, class_id = None):
-        prs = {t : pr for t, pr in zip(iou_thresholds, ap)}
-        mAP = {t : pr.mAP for t, pr in prs.items()}
-
-        class_counts = None
-
-        if None not in [conf_thresholds, class_id]:
-            class_counts = threshold_count(prs[50].confidence, conf_thresholds[class_id]
-                )._extend(truth = target_counts.get(class_id))
+    for i, p in enumerate(prediction._sequence()):
+        match = None
+        if ious.size(1) > 0:
+            iou, j = max_1d(ious[i])
             
+            label = target.label[j]
+            matches_box = iou > threshold
+            
+            if matches_box:
+                ious[:, j] = 0  # mark target overlaps to 0 so they won't be selected twice
+
+                if p.label == label:
+                    match = (j, iou)
+
+        matches.append(p._extend(match = match))
+    return matches
+
+
+
+def rev_cummax(v):
+    flipped = v.flip(0).numpy()
+    rev_max = np.maximum.accumulate(flipped)
+    return torch.from_numpy(rev_max).flip(0)
+
+def area_under_curve(xs, ys):
+    i = (xs[1:] != xs[:-1]).nonzero(as_tuple=False).squeeze(1)
+    return ((xs[i + 1] - xs[i]) * ys[i + 1]).sum().item()
+
+
+
+def compute_mAP(matches, confidence, num_target, weight=1, eps=1e-7):
+
+    false_positives = ((1 - matches) * weight).cumsum(0)
+    true_positives = (matches * weight).cumsum(0)
+
+    recall = true_positives / (num_target if num_target > 0 else 1)
+    precision = true_positives / (true_positives + false_positives).clamp(min = eps)
+
+    recall = bookend(0.0, recall, 1.0)
+    precision = rev_cummax(bookend(1.0, precision, 0.0))
+
+    false_positives = bookend(0, false_positives)
+    true_positives = bookend(0, true_positives)
+
+    return struct(
+        recall = recall, 
+        precision = precision, 
+
+        confidence = bookend(1.0, confidence, 0.0),
+
+        false_positives = false_positives,
+        true_positives = true_positives,
+
+        false_negatives = num_target - true_positives,  
+        n = num_target,
+
+        mAP = area_under_curve(recall, precision))
+
+def mAP_matches(matches, num_target, eps=1e-7):
+    true_positives = torch.FloatTensor([0 if m.match is None else 1 for m in matches])
+    return compute_mAP(true_positives, num_target, eps)
+
+
+def _match_positives(labels_pred, labels_target, ious, threshold=0.5):
+    n, m = labels_pred.size(0), labels_target.size(0)
+    assert ious.size() == torch.Size([n, m]) 
+    ious = ious.clone()
+
+    matches = torch.FloatTensor(n).zero_()
+
+    for i in range(0, n):
+        iou, j = ious[i].max(0)
+        iou = iou.item()
+
+        if iou > threshold:
+            ious[:, j] = 0  # mark target overlaps to 0 so they won't be selected twice
+            if labels_pred[i] == labels_target[j]:
+                matches[i] = 1
+
+    return matches
+
+
+
+def match_positives(detections, target):
+    assert detections.label.dim() == 1 and target.label.dim() == 1
+    n, m = detections._size, target._size
+
+    if m == 0 or n == 0:
+        return const(torch.FloatTensor(n).zero_())
+
+    ious = box.iou_matrix(detections.bbox, target.bbox)
+    return lambda threshold: _match_positives(detections.label, target.label, ious, threshold=threshold)
+
+def list_subset(xs, inds):
+    return [xs[i] for i in inds]
+
+
+def mAP_subset(image_pairs, iou):
+    all_matches =  [match_positives(i.prediction, i.target)(iou) for i in image_pairs]
+
+    def f (inds):
+        pairs = list_subset(image_pairs, inds)
+        confidence    = torch.cat([i.prediction.confidence for i in pairs]).float()
+        confidence, order = confidence.sort(0, descending=True)    
+
+        matches = torch.cat(list_subset(all_matches, inds))[order]
+        n = sum(i.target.label.size(0) for i in pairs)
+
+        return compute_mAP(matches, confidence.cpu(), n)
+    return f
+
+
+def mAP_weighted(image_pairs):
+    confidence    = torch.cat([i.prediction.confidence for i in image_pairs]).float()
+    confidence, order = confidence.sort(0, descending=True)    
+
+    matchers =  [match_positives(i.prediction, i.target) for i in image_pairs]
+    predicted_label = torch.cat([i.prediction.label for i in image_pairs])[order]
+    image_counts = torch.FloatTensor([i.target.label.size(0) for i in image_pairs])
+
+    def f(threshold, image_weights):  
+        matches = torch.cat([match(threshold) for match in matchers])[order]
+
+        def eval_weight(weight):
+            assert weight.size(0) == len(image_pairs)
+            match_weights = torch.cat([torch.full([i.prediction._size], w) 
+                for i, w in zip(image_pairs, weight) ])[order]
+
+            num_targets = torch.dot(image_counts, weight)
+            return compute_mAP(matches, confidence.cpu(), num_targets, weight=match_weights)
+
+        return list(map(eval_weight, image_weights))
+    return f
+
+
+def gaussian_weights(xs, x_eval, sigma):
+    dx2 = (x_eval.unsqueeze(1) - xs).pow(2)
+    return torch.exp(-dx2 / (2*sigma*sigma)) / (math.sqrt(2*math.pi) * sigma)        
+
+
+def mAP_smoothed(image_pairs, xs):
+    assert len(image_pairs) == xs.size(0)
+    weighted_mAP = mAP_weighted(image_pairs)
+
+    def f(threshold, x_eval, sigma):
+        weights = gaussian_weights(xs, x_eval, sigma)
+        results = weighted_mAP(threshold, weights)
+        return torch.Tensor(pluck('mAP', results))
+    return f
+
+
+def mAP_classes(image_pairs, num_classes):
+    confidence    = torch.cat([i.detections.confidence for i in image_pairs]).float()
+    confidence, order = confidence.sort(0, descending=True)    
+
+    matchers =  [match_positives(i.detections, i.target) for i in image_pairs]
+
+    predicted_label = torch.cat([i.detections.label for i in image_pairs])[order]
+    target_label = torch.cat([i.target.label for i in image_pairs])
+
+    num_targets = torch.bincount(target_label, minlength=num_classes)
+    
+    def f(threshold):      
+
+        matches = torch.cat([match(threshold) for match in matchers])[order]
+        def compute_class(i):
+            inds = [(predicted_label == i).nonzero(as_tuple=False).squeeze(1)]
+            return compute_mAP(matches[inds], confidence[inds].cpu(), num_targets[i].item())
+
         return struct(
-            mAP = mAP,
-            AP = mean([ap for k, ap in mAP.items() if k >= 50]),
-
-            thresholds = compute_thresholds(prs[50]),
-            pr50 = condense_pr(prs[50]),
-            pr75 = condense_pr(prs[75]),
-
-            class_counts = class_counts
+            total = compute_mAP(matches, confidence.cpu(),  target_label.size(0)), 
+            classes = [compute_class(i) for i in range(0, num_classes)]
         )
 
-    return struct (
-        total   = summariseAP(info.total),
-        classes = {id : summariseAP(ap, id) for id, ap in zip(class_ids, info.classes)}
-    )
+    return f
 
-
-
-def summarize_test(name, results, classes, epoch, log, thresholds=None):
-
-    class_names = {c.id : c.name for c in classes}
-
-    summary = compute_AP(results, classes, thresholds)
-    total, class_aps = summary.total, summary.classes
-
-    mAP_strs ='mAP@30: {:.2f}, 50: {:.2f}, 75: {:.2f}'.format(total.mAP[30], total.mAP[50], total.mAP[75])
-
-    train_stats = filter_none(pluck('train_stats', results))
-
-    train_summary = summarize_train_stats(name, train_stats, classes, log) \
-        if len(train_stats) > 0 else ''
-
-    print(name + ' epoch: {} AP: {:.2f} mAP@[0.3-0.95]: [{}] {}'.format(epoch, total.AP * 100, mAP_strs, train_summary))
-
-    log.scalars(name, struct(AP = total.AP * 100.0, mAP30 = total.mAP[30] * 100.0, mAP50 = total.mAP[50] * 100.0, mAP75 = total.mAP[75] * 100.0))
-
-    for k, ap in class_aps.items():
-        if ap.class_counts is not None:
-            log.scalars(name + "/counts/" + class_names[k], ap.class_counts)
-
-        log.scalars(name + "/thresholds/" + class_names[k], ap.thresholds)
-
-
-    aps = {class_names[k] : ap for k, ap in class_aps.items()}
-    aps['total'] = total
-
-    for k, ap in aps.items():
-        log.pr_curve(name + "/pr50/" + k, ap.pr50)
-        log.pr_curve(name + "/pr75/" + k, ap.pr75)
-
-    if len(classes) > 1:
-        log.scalars(name + "/mAP50", {k : ap.mAP[50] * 100.0 for k, ap in aps.items()})
-        log.scalars(name + "/mAP75", {k : ap.mAP[75] * 100.0 for k, ap in aps.items()})
-
-        log.scalars(name + "/AP", {k : ap.AP * 100.0 for k, ap in aps.items()})
-
-
-    return total.AP, {k : ap.thresholds for k, ap in class_aps.items()}
