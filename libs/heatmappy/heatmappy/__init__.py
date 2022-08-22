a50e8f7e5174754754fd7b8facb3c1c318b5ce7c
fletcherd3 <fletcherjdick@gmail.com>
2022-08-22 17:26:34 +1200

    Added heatmappy into libs

---
+++ libs/heatmappy/heatmappy/__init__.py
@@ -0,0 +1,4 @@
+from libs.heatmappy.heatmappy.heatmap import Heatmapper,\
+                              GreyHeatMapper,\
+                              PILGreyHeatmapper,\
+                              PySideGreyHeatmapper
